c7e7ed1444b6255675f7b5083c74d22f9763898e
Nicholas Fine <nicholas.fine@gmail.com>
2010-12-18 05:20:23 -0600

    before_filter to get annotation; error responses

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -1,12 +1,21 @@
 class AnnotationsController < ApplicationController
   respond_to :json, :xml
   before_filter :get_book
+  before_filter :get_annotation, :only => [:show]
 
   protected
   def get_book
     @book = Book.find_by_slug(params[:book_id])
     unless @book
       response = {:error => %Q(No book found with that id.  Choose one of #{Book.all.map(&:slug)})}
+      respond_with response and return
+    end
+  end
+
+  def get_annotation
+    @annotation = @book.annotations.show_format.find(params[:id])
+    unless @annotation
+      response = {:error => %(No annotation found with that id.)}
       respond_with response and return
     end
   end
@@ -22,11 +31,10 @@ class AnnotationsController < ApplicationController
   end
 
   def show
-    annotation      = @book.annotations.show_format.find(params[:id])
     urls            = {}
-    urls[:previous] = book_annotation_url(@book, annotation.previous_annotation)
-    urls[:next]     = book_annotation_url(@book, annotation.next_annotation)
-    response        = AnnotationResponse.show_response(annotation, urls)
+    urls[:previous] = book_annotation_url(@book, @annotation.previous_annotation)
+    urls[:next]     = book_annotation_url(@book, @annotation.next_annotation)
+    response        = AnnotationResponse.show_response(@annotation, urls)
     respond_with response
   end
 
