5c7fc78c88a9ec085a47f0204ef449e813d57903
Nicholas Fine <nicholas.fine@gmail.com>
2010-12-17 18:41:39 -0600

    validations

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,3 +1,5 @@
 class Annotation < ActiveRecord::Base
   belongs_to :book
+
+  scope :ordered, order("annotations.page_number, annotations.line_number ASC")
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,10 @@
 class Book < ActiveRecord::Base
   has_many :annotations
+
+  validates_presence_of   :title, :slug, :original_url
+  validates_uniqueness_of :title, :slug, :original_url
+
+  def to_s
+    title
+  end
 end
