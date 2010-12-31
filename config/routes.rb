00e1837788047eef9acd4809fb1319216c1fbd25
Nicholas Fine <nicholas.fine@gmail.com>
2010-12-31 02:16:31 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ AnnotateTheBastards::Application.routes.draw do
   root :to => "books#index"
 
   
-  resources :books, :only => [:index, :show] do
+  resources :books, :only => [:index] do
     resources :annotations, :only => [:index, :show]
   end
   
