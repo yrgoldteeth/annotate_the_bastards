0ee0620ea2c3143b3e086f8ef0e445114cbc29b0
Nicholas Fine <nicholas.fine@gmail.com>
2011-01-17 15:58:48 -0600

    update views

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -1,10 +1,17 @@
 class AnnotationsController < ApplicationController
   respond_to :html, :json, :xml
+  before_filter :setup_pagination_params
   before_filter :get_book
   before_filter :get_annotations
   before_filter :get_annotation, :only => [:show]
 
   protected
+  def setup_pagination_params
+    unless params[:page].present?
+      params[:page] = 1
+    end
+  end
+
   def get_book
     @book = Book.find_by_slug(params[:book_id])
     unless @book
@@ -21,7 +28,7 @@ class AnnotationsController < ApplicationController
     @annotation = @book.annotations.find_by_id(params[:id])
     unless @annotation
       flash[:error] = %(No annotation found with that id.)
-      redirect_to book_annotations_path(@book) and return
+      redirect_to book_annotations_path(@book, :page => params[:page]) and return
     end
   end
 
